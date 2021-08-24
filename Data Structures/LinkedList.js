db7431dce81f237e490c09a4dc32a4b7ab1d31ef
dramos1993 <75751948+dramos1993@users.noreply.github.com>
2021-08-24 02:43:19 -0400

    Add files via upload

---
+++ Data Structures/LinkedList.js
@@ -0,0 +1,89 @@
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null
+    }
+    const leader = this.traverseToIndex(index - 1)
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+
+    return this;
+
+
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const trail = this.traverseToIndex(index + 1);
+    leader.next = trail;
+    this.length--;
+  }
+}
+
+
+const myLinkedList = new LinkedList(5);
+
+myLinkedList.append(16);
+myLinkedList.prepend(10);
+myLinkedList.append(40);
+myLinkedList.printList();
+myLinkedList.insert(2, 99);
+myLinkedList.printList();
+myLinkedList.remove(3)
+myLinkedList.printList()
